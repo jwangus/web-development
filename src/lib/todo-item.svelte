e40d3c50b31fa093419049d974e200475ffe7e93
Jim Wang <jim.wang.nj@gmail.com>
2022-01-08 05:09:21 +0000

    Add GET and POST endpoint

--- src/lib/todo-item.svelte
+++ src/lib/todo-item.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+    export let todo: Todo;
+</script>
+
 <style>
     .todo {
         display: grid;
@@ -62,10 +66,10 @@
 <div class="todo">
     <form action="" method="">
         <input type="hidden" name="done" value="" />
-        <button aria-label="Mark done/not done" class="toggle">Done/Undone</button>
+        <button aria-label="Mark done/not done" class="toggle"></button>
     </form>
     <form action="" method="" class="text">
-        <input type="text" />
+        <input type="text" value="{todo.text}"/>
         <button aria-label="Save todo" class="save"></button>
     </form>
     <form action="" method="">
